79698084da7c161387f7fa6862056337a8ed265c
bartazouhair <bartazouhair@gmail.com>
2023-06-14 05:28:20 -0700

    Commit Task Mandatory

--- 0x04-python-more_data_structures/11-mutiply_list_map.py
+++ 0x04-python-more_data_structures/11-mutiply_list_map.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
+multiply_list_map = __import__('11-multiply_list_map').multiply_list_map
 def multiply_list_map(my_list=[], number=0):
     return (list(map((lambda i: i * number), my_list)))
